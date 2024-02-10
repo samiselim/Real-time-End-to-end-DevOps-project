dd29892651d1bfaec8c93df9b978628de2473838
samiselim <samiselim75@gmail.com>
2024-02-10 17:29:22 +0200

    'fixing some errors'

--- EKS_Cluster/variables.tf
+++ EKS_Cluster/variables.tf
@@ -2,9 +2,11 @@ variable "vpc_cidr" {
   description = "This is cidr of vpc "
   type        = string
 }
-variable "private_subnets" {
-  description = "private Subnets cidr"
-}
 variable "public_subnets" {
+  description = "private Subnets cidr"
+  default = ["192.168.1.0/24", "192.168.2.0/24", "192.168.3.0/24"]
+}
+variable "private_subnets" {
   description = "public Subnets cidr"
+  default = ["192.168.4.0/24", "192.168.5.0/24", "192.168.6.0/24"]
 }
