c08ad28b2bb74889755bd1b69a037dedb30ca367
samiselim <samiselim75@gmail.com>
2024-02-12 02:38:06 +0200

    Fix some errors

--- EKS_Cluster/provider.tf
+++ EKS_Cluster/provider.tf
@@ -1,3 +1,7 @@
 provider "aws" {
   region = "eu-west-3"
 }
+provider "kubernetes" {
+  config_path    = "/var/lib/jenkins/.kube/config"
+  config_context = "arn:aws:eks:eu-west-3:211125306909:cluster/my-eks-cluster"
+}
